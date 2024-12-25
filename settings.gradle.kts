8c48132fe4650cdba3d84edec6e3c13df14f56b1
kidofcubes <kidofcubes@gmail.com>
2024-12-26 01:47:11 +0800

    forgottofixgradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,11 @@
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
-		maven("https://repo.mineinabyss.com/releases")
-		maven("https://repo.mineinabyss.com/snapshots")
-		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-		maven("https://repo.papermc.io/repository/maven-public/") //Paper
 	}
 }
 
 dependencyResolutionManagement {
-	val idofrontVersion: String by settings
-
-	repositories {
-		mavenLocal()
-		maven("https://repo.mineinabyss.com/releases")
-	}
-
 	versionCatalogs {
-		create("libs").from("com.mineinabyss:catalog:$idofrontVersion")
 		create("hiddenlibs").from(files("gradle/hiddenlibs.versions.toml"))
 	}
 }
